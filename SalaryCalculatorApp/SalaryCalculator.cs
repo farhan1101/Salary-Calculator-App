2501695da20d08b9b8a4e156c86ae04a8d8249e1
farhan1101 <farhanshourov@gmail.com>
2015-03-06 12:05:07 +0600

    Initial Commit

---
+++ SalaryCalculatorApp/SalaryCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorApp
+{
+    class SalaryCalculator
+    {
+        public double salary { get; private set; }
+
+        public double GetSalary(double amount, double houseRent, double medAllowance)
+        {
+            return salary = amount + ((amount*houseRent)/100) + ((amount*medAllowance)/100);
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculatorUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorApp
+{
+    public partial class SalaryCalculatorUI : Form
+    {
+        public SalaryCalculatorUI()
+        {
+            InitializeComponent();
+        }
+
+        SalaryCalculator salaryCalc = new SalaryCalculator();
+        private void showMeSalaryButton_Click(object sender, EventArgs e)
+        {
+
+            string name = employeeNameTextBox.Text;
+
+            double amount = Convert.ToDouble(basicAmountTextBox.Text);
+            double houseRent = Convert.ToDouble(houseRentTextBox.Text);
+            double medAllow = Convert.ToDouble(medicalAllowanceTextBox.Text);
+
+            double salary = salaryCalc.GetSalary(amount, houseRent, medAllow);
+
+            MessageBox.Show(name + " ,your salary is: " + salary + " taka");
+
+        }
+    }
+}
+
